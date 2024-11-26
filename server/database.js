2df68836586785a6e4ebc5b06badea36ead405a4
Rizo2901 <114025356+Rizo2901@users.noreply.github.com>
2024-11-26 14:29:04 -0600

    Cambios en databse , para seguridad, y en componentes un poco, ya casi inicio el cambio total de estructura de client

--- server/database.js
+++ server/database.js
@@ -1,22 +1,23 @@
 import dotenv from 'dotenv';
 import sql from 'mssql';
 
+// Cargar variables del entorno desde el archivo .env
 dotenv.config();
-
-const config = {
-    server: process.env.DB_SERVER || 'localhost',
-    port: parseInt(process.env.DB_PORT) || 1433,
-    user: process.env.DB_USER || 'db_connect',
-    password: process.env.DB_PASSWORD || '2901*',
-    database: process.env.DB_NAME || 'Flowly',
-    options: {
-        encrypt: false,
-        trustServerCertificate: true
-    }
-};
 
 async function connectToDatabase() {
     try {
+        const config = {
+            server: process.env.DB_SERVER,
+            port: parseInt(process.env.DB_PORT, 10),
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            options: {
+                encrypt: process.env.DB_ENCRYPT === 'true', // Convertir a booleano
+                trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE === 'true' // Convertir a booleano
+            }
+        };
+
         await sql.connect(config);
         console.log('Successfully connected to SQL Server.');
     } catch (err) {
